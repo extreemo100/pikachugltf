2400999eccb84c82f1896aa9513d99964ed7cf44
extreemo100 <58828956+extreemo100@users.noreply.github.com>
2024-12-14 05:05:00 +0500

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,56 @@
+import {GLTFLoader} from "https://xdsmgyjuwrsmlvtwsjyu.supabase.co/storage/v1/object/public/code%20bucket/GLTFLoader.js?t=2024-12-13T23%3A23%3A40.540Z";
+import * as THREE from "https://xdsmgyjuwrsmlvtwsjyu.supabase.co/storage/v1/object/public/code%20bucket/three.module.js";
+
+//const THREE = window.MINDAR.IMAGE? window.MINDAR.IMAGE.THREE: window.MINDAR.FACE.THREE;
+
+export const loadGLTF = (path) => {
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader();
+    loader.load(path, (gltf) => {
+      resolve(gltf);
+    });
+  });
+}
+
+export const loadAudio = (path) => {
+  return new Promise((resolve, reject) => {
+    const loader = new THREE.AudioLoader();
+    loader.load(path, (buffer) => {
+      resolve(buffer);
+    });
+  });
+}
+
+export const loadVideo = (path) => {
+  return new Promise((resolve, reject) => {
+    const video = document.createElement("video");
+    //video.addEventListener('loadeddata', () => {
+    video.addEventListener('loadedmetadata', () => {
+      video.setAttribute('playsinline', '');
+      resolve(video);
+    });
+    video.src = path;
+  });
+}
+
+export const loadTexture = (path) => {
+  return new Promise((resolve, reject) => {
+    const loader = new THREE.TextureLoader();
+    loader.load(path, (texture) => {
+      resolve(texture);
+    }); 
+  });
+}
+
+export const loadTextures = (paths) => {
+  const loader = new THREE.TextureLoader();
+  const promises = [];
+  for (let i = 0; i < paths.length; i++) {
+    promises.push(new Promise((resolve, reject) => {
+      loader.load(paths[i], (texture) => {
+	resolve(texture);
+      }); 
+    }));
+  }
+  return Promise.all(promises);
+}
